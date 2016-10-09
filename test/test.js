4c5a565e5820ab680d0b5d5ecbc63b39794050fb
Sergej Müller <sergejmueller@users.noreply.github.com>
2016-10-09 19:40:19 +0200

    v0.1.0 (#1)
    ##### New
    * Add `countryEmoji` output
    * Add `.editorconfig` file
    * Add `ESLint` JavaScript linter
    * Add `v0.10` to `.travis.yml`
    
    ##### Changes
    * Core refactoring
    * Code style cleanups
    * Update `mocha` to `v3.1.0`

--- test/test.js
+++ test/test.js
@@ -1,22 +1,23 @@
 "use strict";
 
-var ip2countrify = require('..'),
-    expect = require('chai').expect;
+var ip2countrify = require( '..' ),
+    expect = require( 'chai' ).expect;
 
 
-describe('API requests', function() {
+describe( 'API requests', function() {
 
     /*
         IPv6 Test
     */
 
-    it('IPv6 test', function(done) {
+    it( 'IPv6 test', function( done ) {
         ip2countrify.lookup(
             '2001:420:1101:1::a',
-            function(ip, results, error) {
-                expect(results.countryCode).to.equal('US');
-                expect(results.countryCode3).to.equal('USA');
-                expect(results.countryName).to.equal('United States');
+            function( ip, results ) {
+                expect( results.countryCode ).to.equal( 'US' );
+                expect( results.countryCode3 ).to.equal( 'USA' );
+                expect( results.countryName ).to.equal( 'United States' );
+                expect( results.countryEmoji ).to.equal( '🇺🇸' );
 
                 done();
             }
@@ -28,13 +29,14 @@ describe('API requests', function() {
         IPv4 Test
     */
 
-    it('IPv4 test', function(done) {
+    it('IPv4 test', function( done ) {
         ip2countrify.lookup(
             '5.6.7.8',
-            function(ip, results, error) {
-                expect(results.countryCode).to.equal('DE');
-                expect(results.countryCode3).to.equal('DEU');
-                expect(results.countryName).to.equal('Germany');
+            function( ip, results ) {
+                expect( results.countryCode ).to.equal( 'DE' );
+                expect( results.countryCode3 ).to.equal( 'DEU' );
+                expect( results.countryName ).to.equal( 'Germany' );
+                expect( results.countryEmoji ).to.equal( '🇩🇪' );
 
                 done();
             }
